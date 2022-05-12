3853595657ae6ae3cf1b1066744cb116c5b750d6
Joel Ansbro <joelmatthewansbro@gmail.com>
2022-05-13 00:26:18 +0100

    began API work
    started work on the inbound API to intake JSON data so that it can be saved down locally

--- inboundAPI/api.py
+++ inboundAPI/api.py
@@ -26,3 +26,88 @@
 # keywords array[string]
 
 # unsure at this stage what particular values to enforce, and which could be null (ie: keywords likely null and created later)
+
+# good guide - https://www.digitalocean.com/community/tutorials/processing-incoming-request-data-in-flask
+
+from flask import Flask, request
+from sqlalchemy import null
+
+app = Flask(__name__)
+
+class JsonPayload():
+    # object for the JSONPayload received
+    def __init__(self):
+        self.title = title
+
+
+@app.route('/json-example', methods=['POST'])
+def json_example():
+    request_data = request.get_json()
+
+    title = None
+    author = None
+    project = None
+    date_published = None 
+    lead_image_url = None
+    content = None
+    next_page_url = None 
+    url = None
+    domain = None
+    excerpt = None
+    word_count = None 
+    direction = None
+    total_pages = None 
+    rendered_pages = None  
+
+    if request_data:
+        title = request_data['title']
+        author = request_data['author']
+        project = request_data['project']
+        date_published = request_data['date_published']
+        lead_image_url = request_data['lead_image_url']
+        content = request_data['content']
+        next_page_url = request_data['next_page_url']
+        url = request_data['url']
+        domain = request_data['domain']
+        excerpt = request_data['excerpt']
+        word_count = request_data['word_count']
+        direction = request_data['direction']
+        total_pages = request_data['total_pages']
+        rendered_pages = request_data['rendered_pages']
+
+    return '''
+    title = {}
+    author = {}
+    project = {}
+    date_published = {} 
+    lead_image_url = {}
+    content = {}
+    next_page_url = {} 
+    url = {}
+    domain = {}
+    excerpt = {}
+    word_count = {} 
+    direction = {}
+    total_pages = {} 
+    rendered_pages = {}  
+    '''.format(title,author,project,date_published,lead_image_url,content,next_page_url,url,domain,excerpt,word_count,direction,total_pages,rendered_pages)
+
+def saveJson(response):
+    #save down JSON locally
+    return null
+
+
+def sanitisaton(response):
+    #sanitise input
+    return null
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
+
+
+# look to ticket to see further, but in a nutshell:
+# need to refactor
+# store the params within a good data structure
+
+
--- outboundAPI/api.py
+++ outboundAPI/api.py
@@ -2,3 +2,26 @@
 # this api desginates endpoints for interacting with the database to pull report data through
 
 # contains code that implements the user interface
+
+
+import json
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/users')
+def get_users():
+    users = [
+        {
+            'name':'exampleUser',
+            'display_name': 'Jane Doe',
+            'email': 'user@example.com'
+        }
+    ]
+    return json.dumps(users)
+
+
+if __name__ == '__main__':
+    app.run()
+
