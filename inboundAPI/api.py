ea7a6c774c8d1aeeb2bdb20a85d13bd6aca66034
Joel Ansbro <joelmatthewansbro@gmail.com>
2022-05-12 19:03:19 +0100

    update notes
    updated notes on the inbound API

--- inboundAPI/api.py
+++ inboundAPI/api.py
@@ -3,3 +3,27 @@
 # inbound data from this api needs to be appended with an identifier to point out what scraping job it belongs to
 
 # contains code that implements the external APIs
+
+# we dont at this stage have to worry about assigning a unique identifier here, 
+# as The ObjectID is the primary key for the stored document and is automatically generated when creating a new document in a collection
+# https://www.mongodb.com/docs/manual/reference/method/ObjectId/
+# this project must create the JSON in the following schema:
+
+# title string
+# author string
+# project - individual project assignment for report tracking string
+# date_published int
+# dek unknown at this point I should really look into what this means
+# lead_image_url string
+# content string
+# next_page_url string
+# url string
+# domain string
+# excerpt string
+# word_count int
+# direction string
+# total_pages int
+# rendered_pages int
+# keywords array[string]
+
+# unsure at this stage what particular values to enforce, and which could be null (ie: keywords likely null and created later)
