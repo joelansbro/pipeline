66bb1321050ad79685098ac831af542df52b39bd
jagithub2 <jagithub@proton.me>
2023-03-03 17:09:51 +0000

    Change test scripts to accomodate API route changes

--- testout.py
+++ testout.py
@@ -6,7 +6,7 @@ import json
 title = "From Python to Numpy"
 title = title.replace(" ", "%20")
 
-res = requests.get('http://localhost:5050/outbound/get_article/{}'.format(title))
+res = requests.get('http://localhost:5000/outbound/get_article/{}'.format(title))
 
 if res.ok:
     print(res.content)
